e570401ffa8b42c9d3924c0de121a53db9c88520
andres <1908@holbertonschool.com>
2020-06-15 09:29:45 +0000

    added the file 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+  * main - prinf
+  *
+  *Return: print requested message (success)
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
