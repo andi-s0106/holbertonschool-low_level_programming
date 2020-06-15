ecf5fe91d1b4c5ecb23e4e8f3a0d82072006a1ba
andres <1908@holbertonschool.com>
2020-06-15 09:17:46 +0000

    added the file 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include(string.h)
+/**
+  * main - print message
+  *
+  *Return: print requested message (success)
+  */
+
+int main(void)
+{
+	char str1[54] = "\"Programming is like building a multilingual puzzle\n";
+
+	puts(str1);
+
+	return (0);
+}
