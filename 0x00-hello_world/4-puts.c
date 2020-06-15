d12bf829cf5f75d2e76123ed1cb132682081bf38
andres <1908@holbertonschool.com>
2020-06-15 09:23:46 +0000

    updated the file 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-	char str1[54] = "\"Programming is like building a multilingual puzzle\n";
+	char str1[54] = "\"Programming is like building a multilingual puzzle";
 
 	puts(str1);
 
