32bbf28e4acc9b5409949ec41a57d74eeb8016dc
andi-s0106 <andress.933110@gmail.com>
2020-07-16 20:45:34 +0000

    added the file 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+  * main - prints alphabet in lower and uppercase
+  *
+  * Return: Alphabet in lower and uppercase
+  */
+int main(void)
+{
+	char a;
+
+	for (a ='a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	for (a = 'A'; a <= 'Z'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
