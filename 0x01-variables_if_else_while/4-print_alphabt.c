362cdd9581cc71ebd2cde6f12b918f01f819a79e
andi-s0106 <andress.933110@gmail.com>
2020-07-16 21:42:33 +0000

    added the file 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+  * main - print alphabet excluding 'q' and 'e'
+  *
+  * Return: alphabet in lowercase excluding 'q' and 'e'
+  */
+int main (void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a == 'e')
+		{}
+		else if (a == 'q')
+		{}
+		else
+		{
+			putchar(a);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
