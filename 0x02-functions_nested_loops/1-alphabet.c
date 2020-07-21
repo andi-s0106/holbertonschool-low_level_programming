cbb49bbea4395a18473a3d1096338d641fd81ef9
andi-s0106 <andress.933110@gmail.com>
2020-07-21 16:21:25 +0000

    added the file 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - prints alphabet
+ *
+ * Return: alphabet
+ */
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+}
