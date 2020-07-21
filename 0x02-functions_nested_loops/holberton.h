1cc3e0947b4dd386794d7b6bd49a05d5574bb946
andi-s0106 <andress.933110@gmail.com>
2020-07-21 15:33:41 +0000

    updated the file holberton.h

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,1 +1,12 @@
-int _putchar(char c)
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+int _putchar(char c);
+
+
+#endif /*Header holberton.h*/
