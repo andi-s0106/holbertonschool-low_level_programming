d33c572f6a748584d3ac947673ca4f9c364b22f7
andi-s0106 <andress.933110@gmail.com>
2020-07-21 15:51:31 +0000

    corrected betty issues with file 0-holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -2,32 +2,32 @@
 
 /**
  * main - prints holberton followed by a new line
- * 
+
  * Return: Holberton\n
  */
 
 int main(void)
 {
-    char c;
+	char c;
 
-    c = 'H';
-    _putchar(c);
-    c = 'o';
-    _putchar(c);
-    c = 'l';
-    _putchar(c);
-    c = 'b';
-    _putchar(c);
-    c = 'e';
-    _putchar(c);
-    c = 'r';
-    _putchar(c);
-    c = 't';
-    _putchar(c);
-    c = 'o';
-    _putchar(c);
-    c = 'n';
-    _putchar(c);
-    _putchar('\n');
-    return (0);
+	c = 'H';
+	_putchar(c);
+	c = 'o';
+	_putchar(c);
+	c = 'l';
+	_putchar(c);
+	c = 'b';
+	_putchar(c);
+	c = 'e';
+	_putchar(c);
+	c = 'r';
+	_putchar(c);
+	c = 't';
+	_putchar(c);
+	c = 'o';
+	_putchar(c);
+	c = 'n';
+	_putchar(c);
+	_putchar('\n');
+	return (0);
 }
