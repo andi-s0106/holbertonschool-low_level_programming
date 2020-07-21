80230fe9e9abe95d08382a8f4f4e4fd2e44a990a
andi-s0106 <andress.933110@gmail.com>
2020-07-21 08:40:57 +0000

    added the file 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+  * main - prints single digit numbers separated by a comma and space
+  *
+  * Return: single digit numbers followed by a comma and space
+  */
+int main(void)
+{
+	int n;
+	for (n = 0; n <= 9; n++)
+	{
+		putchar('0' + n);
+		if (n >= 0 && n < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
