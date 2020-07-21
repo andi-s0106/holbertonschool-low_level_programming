77aa4b8654a93d447d45a4587612cf55b2a2ff8b
andi-s0106 <andress.933110@gmail.com>
2020-07-21 18:04:04 +0000

    added the definition for the function _abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * _abs - Gets the absolute value of an int
+ * @n: the number to be checked
+ * Return: the absolute value of n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = n * -1;
+	}
+	return(n);
+}
