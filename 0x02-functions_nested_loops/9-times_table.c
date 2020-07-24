7abaeb836dfaee294a06dcca0aa3cdb5ca4c9049
andi-s0106 <andress.933110@gmail.com>
2020-07-24 21:35:21 +0000

    added the prototype for the func times_table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+/**
+ * times_table: prints the mult table from 0 to 9
+ */
+
+void times_table(void)
+{
+	int n;
+	int m;
+	int r;
+
+	for ( n= 0; n <= 9; n++)
+	{
+		for (m = 0; m <= 9; m++)
+		{
+			r = n * m;
+			if (r <= 9)
+			{
+				_putchar('0' + r);
+			}
+			else
+			{
+				_putchar('0' + (r / 10));
+				_putchar('0' + (r % 10));
+			}
+			if (m < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (r <= 9)
+			{
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
