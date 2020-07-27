f294d7b13b26ca6ad22e6f41b350ddf00e66468b
andi-s0106 <andress.933110@gmail.com>
2020-07-27 04:13:40 +0000

    added the def for function add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * add - adds two numbers
+ * @a: first number
+ * @b: second number
+ * Return: result of the operation
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
