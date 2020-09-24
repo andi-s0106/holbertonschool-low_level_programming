741de5b44e1af1a7a45f9d408261165e5eb03cb5
andi-s0106 <andress.933110@gmail.com>
2020-09-24 23:58:27 +0000

    added the file 2-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ *print_numbers - print number 0 to 9
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for(n = 0; n < 10; n++)
+	{
+		_putchar(n + 48);
+	}
+	_putchar('\n');
+}
