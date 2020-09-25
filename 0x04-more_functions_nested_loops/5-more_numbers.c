9c2ec988508771bbbd66f79c061385c56775858c
andi-s0106 <andress.933110@gmail.com>
2020-09-25 00:38:31 +0000

    updated the file 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,13 +11,14 @@ void more_numbers(void)
 	{
 		for (n = 0; n < 15; n++)
 		{
-			if (n > 10)
+			if (n <= 9)
 			{
-				_putchar(n + 48);
+				_putchar(n + '0');
 			}
 			else
 			{
-				_putchar(n % 10 + 48);
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
 			}
 		}
 		_putchar('\n');
