ea76f9d9e3a95ae13ec73c8afd79992ba3acf1d5
andi-s0106 <andress.933110@gmail.com>
2020-09-30 02:18:48 +0000

    added the file 5-rev_strings.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ *rev_string - reverses a string
+ *@s: string to be reversed
+ */
+void rev_string(char *s)
+{
+	int len;
+	int head;
+	char tmp;
+
+	for (len = 1; s[len]; len++)
+	{}
+
+	for (head = 0; head < len; head++, len--)
+	{
+		tmp = s[len - 1];
+		s[len - 1] = s[head];
+		s[head] = tmp;
+	}
+}
