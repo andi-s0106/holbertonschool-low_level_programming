cf029d94269b1ba6890de663443f1aafe447b0ed
andi-s0106 <andress.933110@gmail.com>
2020-10-02 02:19:04 +0000

    added the file 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ *_strcat - concatenates two strings
+ *@dest: destination string
+ *@stc: string to be appended
+ *Return: pointer to the resulting string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int len;
+	int i;
+
+	for (len = 0; dest[len] != '\0'; len++)
+	{}
+
+	for (i = 0; src[i] != '\0'; i++, len++)
+	{
+		dest[len] = src[i];
+	}
+	dest[len] = '\0';
+
+	return (dest);
+}
