89581cce0106cc4079eefb5262aca07cc89899c9
andi-s0106 <andress.933110@gmail.com>
2020-10-06 20:20:11 +0000

    added the file 0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ *_memset - fills memory with a constant byte
+ *@s: pointer
+ *@b: constant byte
+ *@n: first bytes of memory
+ *Return: pointer to the memory area of s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
