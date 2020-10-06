9e37669d8431b74ee8470d2a448302979598bb60
andi-s0106 <andress.933110@gmail.com>
2020-10-06 20:36:05 +0000

    added 3-strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+/**
+ *_strspn - gets the length of a prefix substring
+ *@s: string
+ *@accept: substring
+ *Return: number of bytes in the initial segment of s which consist
+ *only of bytes from accpet
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int l;
+	unsigned int c;
+	unsigned int counter;
+
+	counter = 0;
+	for (c = 0; accept[c] != '\0'; c++)
+	{
+		for (l = 0; s[l] != '\0'; l++)
+		{
+			if (accept[c] == s[l])
+				counter++;
+			/*Stop counting when not a character*/
+			if (s[l + 1] < 'A' || s[l + 1] > 'Z')
+			{
+				if (s[l + 1] < 'a' || s[l + 1] > 'z')
+					break;
+			}
+
+		}
+	}
+	return (counter);
+}
