33ce456df08c4138ac562eaba8a99652d077a74f
andi-s0106 <andress.933110@gmail.com>
2020-10-09 02:24:19 +0000

    added 4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ *_pow_recursion - returns the value of x to the power of y
+ *@x: number to be processed
+ *@y: potenciation
+ *Return: the value of x to the power of y, -1 if 7 < 0
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < -1)
+		return (-1);
+	if (y == 0)
+		return (1);
+	if (y <= 1)
+		return (x);
+	return (x * _pow_recursion(x, y - 1));
+}
--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -13,5 +13,6 @@ char *rev_string(char *s);
 int _strlen_recursion(char *s);
 int len(char *s, int counter);
 int factorial(int n);
+int _pow_recursion(int x, int y);
 
 #endif /*Header holberton.h*/
