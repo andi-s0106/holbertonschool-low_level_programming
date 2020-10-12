4f28bdeacdab25b26aa97f0bc6c12c1e4538ed21
andi-s0106 <andress.933110@gmail.com>
2020-10-12 01:49:26 +0000

    added 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ *main - Entry point
+ *@argc: number of parameters
+ *@argv: An array with the parameters passed to the function
+ *Return: Aways zero
+ */
+int main(int argc, __attribute__((unused)) char **argv)
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
--- 0x0A-argc_argv/holberton.h
+++ 0x0A-argc_argv/holberton.h
@@ -8,6 +8,7 @@
 
 int _putchar(char c);
 int main(__attribute__((unused))int argc, char **argv);
+int main(int argc, __attribute__((unused)) char **argv);
 
 
 #endif /*Header holberton.h*/
