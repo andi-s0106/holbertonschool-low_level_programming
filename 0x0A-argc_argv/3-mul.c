dc7e7ff798953f02dce2a9ccf457f0febf3f103b
andi-s0106 <andress.933110@gmail.com>
2020-10-13 04:23:12 +0000

    added 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ *main - Entry point
+ *@argc: number of parameters
+ *@argv: An array with the parameters passed to the function
+ *Return: zero if sucessful 1 if there is less than 2 arguments
+ */
+int main(int argc, __attribute__((unused))char **argv)
+{
+	int mult;
+
+	if (argc <= 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	mult = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", mult);
+	return (0);
+}
--- 0x0A-argc_argv/holberton.h
+++ 0x0A-argc_argv/holberton.h
@@ -10,6 +10,6 @@ int _putchar(char c);
 int main(__attribute__((unused))int argc, char **argv);
 int main(int argc, __attribute__((unused)) char **argv);
 int main(__attribute__((unused))int argc, char **argv);
-
+int main(int argc, __attribute__((unused))char **argv);
 
 #endif /*Header holberton.h*/
